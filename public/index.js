36a26efea873c8025b2efff810f8d9a79515d307
Divyansh <divyanshm1609@gmail.com>
2021-10-12 19:02:33 +0530

    Styling and progress bar

--- public/index.js
+++ public/index.js
@@ -6,6 +6,8 @@ const sendlink=document.querySelector('.send-link');
 const show=document.querySelector('.show');
 const showMail=document.querySelector('.showMail');
 const copybtn=document.querySelector('.copy-btn');
+const status=document.querySelector('.status');
+const percentage=document.querySelector('.percentage');
 
 dropZone.addEventListener("dragover", function(e) {
     e.preventDefault();
@@ -44,18 +46,23 @@ const uploadFile=()=>{
 
     const xhr=new XMLHttpRequest();
     xhr.onreadystatechange=()=>{
+        if(xhr.readyState==XMLHttpRequest.OPENED){
+            status.style.display='block';
+        }
         if(xhr.readyState==XMLHttpRequest.DONE){
-            console.log(JSON.parse(xhr.response));
             const url=JSON.parse(xhr.response).file;
             showlink.value=url;
             sendlink.value=url;
+            status.style.display='none';
         }
     }
     xhr.upload.onprogress=updateProgress;
-    xhr.open("POST","https://bigshare.herokuapp.com/api/files");
+    xhr.open("POST","http://localhost:8080/api/files");
     xhr.send(formData);
 }
 function updateProgress(e){
+    let percent=Math.floor((e.loaded/e.total)*100);
+    percentage.innerHTML=percent;
     if(e.loaded==e.total){
         show.style.display="block";
         showMail.style.display="block";
